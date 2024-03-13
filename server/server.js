edd67069ce193cf87849c185699bab91befd3b3d
Caleb C <kaichun24@gmail.com>
2024-03-13 12:06:11 -0700

    default path '/' sends status message, '/users' gets all users

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
 require('dotenv').config();
 
 const express = require('express');
+
+// express app
 const app = express();
+
+// database
 const connection = require('./database');
 
+// test db connection
 app.get('/', function (req, res) {
     let sql = "SELECT * FROM initial_broncohacks_db.User"
     connection.query(sql, function (err, result) {
-        if (err) throw err;
-        res.send(result)
+        if (err) {
+            // 502 Bad Gateway 
+            res.status(502).send('Failed To Connect')
+        } else {
+            // 200 OK
+            res.status(200).send('Successful Connection')
+        }
     })
 });
 
+// gets all routes from ./routes/Users
+const usersRouter = require('./routes/Users');
+app.use("/users", usersRouter);
+
+
 app.listen(process.env.PORT, () => {
     console.log('app listening on port', process.env.PORT)
+
+    // connect to db
     connection.connect(function(err) {
         if (err) throw err;
         console.log('database connected')
