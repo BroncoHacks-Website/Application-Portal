f9ec489e813b9b5b87e618257a43abdd2026896d
Caleb C <kaichun24@gmail.com>
2024-03-12 18:43:50 -0700

    set up server and connected to db

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const connection = require('./database');
+
+app.get('/', function (req, res) {
+    let sql = "SELECT * FROM initial_broncohacks_db.User"
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        res.send(result)
+    })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('app listening on port', process.env.PORT)
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log('database connected')
+    })
+});
