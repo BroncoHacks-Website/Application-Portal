249b29af3bdfdaeb3eee75c08bd550e51076948d
Cesar de Paula <cdepaula942@gmail.com>
2024-03-17 01:08:42 -0700

    added link to CreateAccount page

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -8,14 +8,19 @@ function Login() {
 
     return(
         <>
-            <form action="">
-                <label htmlFor="email">Email</label>
-                <input type="text" className="email" />
-                <label htmlFor="password">Password</label>
-                <input type="password" className="password" />
-                <input type="submit" className="submit" value="submit"/>
-            </form>
-            
+            <div className="container-login">
+                <form action="">
+                    <h2>Welcome</h2>
+                    <label htmlFor="email">Email</label>
+                    <input type="text" className="email" />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="password" />
+                    <input type="submit" className="submit" value="submit"/>
+                </form>
+            </div>
+            <div className="container-signUp">
+                <a href="/">Not a User? Sign Up</a>
+            </div>
         </>
     );
 }
