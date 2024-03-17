80808b85dfd54bab2bee0c15657e077ac6014420
Nicholas Amancio <nick.amancio44@gmail.com>
2024-03-17 00:30:03 -0700

    This is for CESAR'S eyes only...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/login.jsx'
 
 function App() {
     return (
         <>
-            
+            <Login></Login>
         </>
     )
 }
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,8 +1,20 @@
 import { useState } from "react";
 
 function Login() {
+    const [email, password] = useState({
+        email : "",
+        password : "",
+    });
+
     return(
         <>
+            <form action="">
+                <label htmlFor="email">Email</label>
+                <input type="text" className="email" />
+                <label htmlFor="password">Password</label>
+                <input type="password" className="password" />
+                <input type="submit" className="submit" value="submit"/>
+            </form>
             
         </>
     );
