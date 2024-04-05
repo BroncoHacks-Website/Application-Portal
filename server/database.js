3829e5701f4f0f232fadb9d21910c922450bd792
Gerardo Solis <gsolis5114@gmail.com>
2024-04-05 10:42:11 -0700

    updated database.js user resolving to host user and not .env user

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connection = mysql2.createConnection({
     host: process.env.HOST,
     database: 'initial_broncohacks_db',
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD
 }).promise();
 
