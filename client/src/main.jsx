0e3d09025ece4726a77b2b0b09617799214683fe
Nicholas Amancio <nick.amancio44@gmail.com>
2024-04-05 23:16:26 -0700

    moved around the Team Page in the new merged files from Jerry's Branch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,6 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <App></App>
-=======
-      <App />
->>>>>>> 6a526513bd40252e297b6c61d7ec18a9e1a60788
   </React.StrictMode>,
 )
