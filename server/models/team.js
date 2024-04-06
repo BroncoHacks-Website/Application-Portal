f1217ecf11335ff7e1f70281a271bed832fdc78f
Gerardo Solis <gsolis5114@gmail.com>
2024-04-05 17:08:47 -0700

    Updated user.js and team.js

--- server/models/team.js
+++ server/models/team.js
@@ -1,15 +1,37 @@
 // import the database connection
+const { check } = require("express-validator");
 const db = require("../database");
 
+// ---------------- Creation ----------------
 async function createTeam(teamName, passcode, teamOwnerId) {
+    if(await checkTeamNameExists(teamName)) {
+        throw new Error('Team name is Taken');
+    }
+    if(await checkUserInAnyTeam(userId)) {
+        throw new Error('User is already part of a team');
+    }
+    const passcode = generatePasscode();
     const [team] = await db.query(
         'INSERT INTO Team(teamName, passcode, teamOwnerId) VALUES(?,?,?)', [teamName, passcode, teamOwnerId]
     );
-
-    const id = team.insertId;
-    return getTeam(id);
+    
+    await addTeamMember(team.insertId, teamOwnerId, true);
+    return getTeam(team.insertId);
 }
 
+async function generatePasscode() {
+    let randomString = Math.random().toString(36).substring(2);
+    randomString = randomString.padEnd(10, '0').substring(0, 8);
+}
+
+// ---------------- Update ----------------
+async function addTeamMember(teamId, userId, isOwner) {
+    await db.query('INSERT INTO TeamMember(teamid, userid, isOwner) VALUES(?,?,?)',
+        [teamId, userId, isOwner]
+    );
+}
+
+// ---------------- Checks ----------------
 async function checkTeamNameExists(teamName) {
     const [teams] = await db.query('SELECT * FROM Team WHERE teamName = ?',
         [teamName]
@@ -18,6 +40,13 @@ async function checkTeamNameExists(teamName) {
     return teams.length > 0;
 }
 
+async function checkUserInAnyTeam(userId) {
+    const [members] = await db.query('SELECT * FROM TeamMember Where userid = ?', 
+        [userId]);
+    return members.length > 0;
+}
+
+// ---------------- Retrieval ----------------
 async function getTeams() {
     const [teams] = await db.query('SELECT * FROM Team');
     return teams;
@@ -28,16 +57,17 @@ async function getTeam(id) {
     return team;
 }
 
+// ---------------- Delete ----------------
 async function deleteTeam(id) {
     const team = await getTeam(id);
     await db.query('DELETE FROM Team WHERE teamid = ?', [id]);
-    return team;
+    return team.length ? team[0] : null;
 }
 
+//  ---------------- Exports ----------------
 module.exports = { 
     createTeam,
     checkTeamNameExists,
     getTeams,
     deleteTeam,
-
 };
