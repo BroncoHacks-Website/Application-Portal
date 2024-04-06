a6e1a4b21a2ace83678d805f3a23a81934508d8e
Gerardo Solis <gsolis5114@gmail.com>
2024-04-05 19:28:46 -0700

    created router for teams

---
+++ server/routes/teams.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const TeamController = require("../controllers/team");
+const {teamIdValidator, teamCreationValidator} = require("../validators/teams");
+
+// Get route to get all teams
+router.get("/", TeamController.getAllTeams);
+
+// get route to get team by id
+router.get("/:teamid", teamIdValidator, TeamController.getTeamByID);
+
+// POST route to create new team
+router.post("/", teamCreationValidator, TeamController.createTeam);
+
+// DELETE route to delete a team
+router.delete("/:teamid", teamIdValidator, TeamController.deleteTeam);
+
+module.exports = router;
