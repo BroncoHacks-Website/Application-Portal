8296fcd54460c2b36a15d175977b9e49b03b27d9
Nicholas Amancio <nick.amancio44@gmail.com>
2024-04-05 23:33:12 -0700

    added the create/join team front end to the page. Still need to work on the css and connection to backend

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,5 +1,25 @@
+import { useState} from "react";
+import '../styles/team.css';
+
 const Team = () => {
-    return <div>Team Page</div>;
+    return <>
+        <body>
+            <div className="create-team-container">
+                <h1 className="enter-team-name">Create Your Team</h1>
+                <div className="team-name-container">
+                    <p className="enter-team-name-prompt">Enter your team name: </p>
+                    <input className="enter-team-name-input" type="text" placeholder="Team Name"/>
+                </div>
+                <input className="enter-team-name-submit" type="submit" value="Create New Team!"/>
+                <a className="create-team-go-back" href="/">Back to Main Menu</a>
+            </div>
+            <div className="join-team-container">
+                <h1 className="join-team-name">Join a Team via Code</h1>
+                <input className="join-team-name-input" type="text" placeholder="Example: ab12Cd3E"/>
+                <input className="join-team-name-submit" type="submit" value="Join" />
+            </div>
+        </body>
+    </>;
   };
   
   export default Team;
