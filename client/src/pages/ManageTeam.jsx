d5c9221b791e113b2edfde96a06cfcf557ea10b3
Caleb C <kaichun24@gmail.com>
2024-04-14 17:49:18 -0700

    basic manage team page css

---
+++ client/src/pages/ManageTeam.jsx
@@ -0,0 +1,46 @@
+import '../styles/ManageTeam.css'
+
+import { useState, useEffect } from 'react'
+
+const TeamMember = ({nm}) => {
+
+    const [memberName, setMemberName] = useState(nm)
+
+    return (
+        <div id='team-member'>
+            <p>Member: {memberName}</p>
+            <button className='member-btn' id='view-profile'>View Profile</button>
+            <button className='member-btn' id='make-owner'>Make Owner</button>
+            <button className='member-btn' id='kick-member'>Kick Member</button>
+        </div>
+    )
+}
+
+const ManageTeam = () => {
+
+    // all the variables here are just filler
+    const [teamName, setTeamName] = useState("quandavius winglebinglers")
+    const [accessCode, setAccessCode] = useState("jAF75F1")
+    const [owner, setOwner] = useState("lebrawn james")
+    const [mem1, setMem1] = useState("anthony davis")
+    const [mem2, setMem2] = useState("austin reaves")
+    const [mem3, setMem3] = useState("")
+
+    useEffect(() => {
+        
+    }, [])
+
+    return(<div className='page'>
+        <div className='container'>
+            <h1 id='team-name'>Team Name: {teamName}</h1>
+            <p id='access-code'>Access Code: {accessCode}</p>
+            <div id='owner'>Owner: {owner}</div>
+            <TeamMember nm={mem1}></TeamMember>
+            <TeamMember nm={mem2}></TeamMember>
+            <TeamMember nm={mem3}></TeamMember>
+            <button id='delete-team'>Delete Team</button>
+        </div>
+    </div>)
+}
+
+export default ManageTeam;
