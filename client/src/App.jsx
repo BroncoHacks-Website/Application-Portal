d57846f4cf98d81c8e575a7920f308967304e842
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-04-28 22:16:55 -0700

    Moved ManageTeam page to directory folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
-import {useState} from "react";
+import { useState } from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 import Team from "./pages/Team.jsx";
 import FAQ from "./pages/FAQ.jsx";
 import Signup from "./pages/Signup.jsx";
-import ManageTeam from "./pages/ManageTeam.jsx";
+import ManageTeam from "./pages/directory/ManageTeam.jsx";
 
 import Navbar from "./components/Navbar.jsx";
 
-
 function App() {
   // const [count, setCount] = useState(0)
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
 
   return (
     <>
       <BrowserRouter>
-        <Navbar toggle={toggle} setToggle={setToggle}/>
+        <Navbar toggle={toggle} setToggle={setToggle} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/team" element={<Team />} />
