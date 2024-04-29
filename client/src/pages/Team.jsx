7aa2905de93bd358f11836397cb86431a06b68e7
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-04-28 22:29:41 -0700

    Manage Team button on Team page

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -1,6 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const Team = () => {
-    return <div>Team Page</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/ManageTeam");
   };
-  
-  export default Team;
-  
+
+  return (
+    <div className="container">
+      <div className="teamPageHeader">
+        <h1>Team Page</h1>
+      </div>
+      <div className="manageTeamButton">
+        <button onClick={handleClick}>Manage Team</button>
+      </div>
+    </div>
+  );
+};
+
+export default Team;
